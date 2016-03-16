e69b32fff8f91d61a20183f927fb96cfac6f95d3
HanselHu <apologize0616@gmail.com>
2016-03-17 00:10:20 +0800

    作業批改完成

--- RectangleTester.java
+++ RectangleTester.java
@@ -1,3 +1,11 @@
+
+/*
+ * [A]100
+ * [TA's advise]
+ * 1.Good Job!
+ * 2.能和同學們一起討論是進步的開始, 希望你可以慢慢地能夠自己完成程式甚至成為能教別人的人! 加油!
+ * */
+
 import java.util.Scanner;
 
 class Rectangle {
@@ -7,15 +15,14 @@ class Rectangle {
 	double y;
 
 	Rectangle() {
-		this (0,0,0,0);
+		this(0, 0, 0, 0);
 	}// important-
-	
+
 	Rectangle(double _height, double _width, double _x, double _y) {
 		this.height = _height;
 		this.width = _width;
 		this.x = _x;
 		this.y = _y;
-
 	}
 
 	double getHeight() {
@@ -33,49 +40,49 @@ class Rectangle {
 	double getY() {
 		return this.y;
 	}
+
 	public String toString() {
-		return "java.Rectangle[x="+this.x+" y="+this.y+" height:"+ this.height+" 5width"+ this.width+"]";
+		return "java.Rectangle[x=" + this.x + " y=" + this.y + " height:" + this.height + " 5width" + this.width + "]";
 	}
 }
-public class RectangleTester{
+
+public class RectangleTester {
 
 	public static void main(String[] args) {
-		
+
 		Scanner in = new Scanner(System.in);
-		double h,w,x,y;
+		double h, w, x, y;
 		System.out.println("java rec1");
 		System.out.println("height: ");
 		h = in.nextDouble();
 		System.out.println("width: ");
 		w = in.nextDouble();
-		
+
 		System.out.println("x:");
 		x = in.nextDouble();
 		System.out.println("y:");
 		y = in.nextDouble();
-		
-		Rectangle rec1=new Rectangle(h,w,x,y);
+
+		Rectangle rec1 = new Rectangle(h, w, x, y);
 		System.out.println(rec1.toString());
-		System.out.println("Area="+rec1.getHeight()*rec1.getWidth());
-		System.out.println("Perimeter="+(rec1.getHeight()+rec1.getWidth())*2);
-	
+		System.out.println("Area=" + rec1.getHeight() * rec1.getWidth());
+		System.out.println("Perimeter=" + (rec1.getHeight() + rec1.getWidth()) * 2);
+
 		System.out.println("java rec2");
 		System.out.println("height: ");
 		h = in.nextDouble();
 		System.out.println("width: ");
 		w = in.nextDouble();
-		
+
 		System.out.println("x:");
 		x = in.nextDouble();
 		System.out.println("y:");
 		y = in.nextDouble();
-		
-		Rectangle rec2=new Rectangle(h,w,x,y);
+
+		Rectangle rec2 = new Rectangle(h, w, x, y);
 		System.out.println(rec2.toString());
-		System.out.println("Area="+rec2.getHeight()*rec2.getWidth());
-		System.out.println("Perimeter="+(rec2.getHeight()+rec2.getWidth())*2);
-	
-	}
-	}
+		System.out.println("Area=" + rec2.getHeight() * rec2.getWidth());
+		System.out.println("Perimeter=" + (rec2.getHeight() + rec2.getWidth()) * 2);
 
-
+	}
+}
